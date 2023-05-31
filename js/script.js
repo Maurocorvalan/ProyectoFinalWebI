14ee40458dc3dae44e44bc403e4fa45a39ecd80c
Mauro Corvalan <maurocorvaa@gmail.com>
2023-05-31 19:05:54 -0300

    css y js
    Agregada validacion del formulario y añadiendo estilos css

--- js/script.js
+++ js/script.js
@@ -33,3 +33,74 @@ function moverAtras(){
     document.fotoCarrousel.src = imagenes[cont];
 }
 btnIzquierdo.addEventListener("click",moverAtras);
+
+function validar(){
+    //Expresion regular de un email
+    let exregEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    //Expresion regular de 8 digitos para el telefono
+    let exregTelf = /^\d{8}$/;
+    let sinError = true;
+    let nombre = document.getElementById("nombre");
+    let email = document.getElementById("email");
+    let telefono = document.getElementById("telef");
+    let recomendaciones = document.getElementById("recomend");
+    let ltaErrores = document.getElementById("ltaErrores");
+    let ltaMensajes = document.getElementById("ltaMensajes");
+
+    ltaErrores.innerHTML = "";
+    ltaMensajes.innerHTML = "";
+    
+    nombre.classList.remove("error");
+    recomendaciones.classList.remove("error");
+    email.classList.remove("error");
+    telefono.classList.remove("error");
+
+    if(nombre.value.trim()==""){
+        let item = document.createElement("li");
+         ltaErrores.appendChild(item);
+        item.innerHTML = "nombre invalido";
+        nombre.classList.add("error");
+        sinError=false;
+        nombre.focus();
+    }
+
+    if(!exregEmail.test(email.value)){
+        let item = document.createElement("li");
+         ltaErrores.appendChild(item);
+        item.innerHTML = "Email invalido";
+        email.classList.add("error");
+        sinError=false;
+        email.focus();
+    }
+    if(!exregTelf.test(telefono.value)){
+        let item = document.createElement("li");
+        ltaErrores.appendChild(item);
+        item.innerHTML = "telefono invalido";
+        telefono.classList.add("error");
+        sinError=false;
+        telefono.focus();
+    }
+    if(recomendaciones.value.trim()=="" || recomendaciones.value.length > 50){
+        let item = document.createElement("li");
+         ltaErrores.appendChild(item);
+        item.innerHTML = "mensaje invalido";
+        recomendaciones.classList.add("error");
+        sinError=false;
+        recomendaciones.focus();
+    }
+    if(sinError){
+        let item = document.createElement("li");
+         ltaMensajes.appendChild(item);
+        item.innerHTML = nombre.value+
+        "<p></p>"+"Email:  "+
+        email.value+
+        "<p></p>"+
+        "Tu recomendacion:  "+
+        recomendaciones.value+
+        " <p></p>Se registro.";
+        recomendaciones.value = "";
+        recomendaciones.focus();
+    }
+
+    return false;
+}
